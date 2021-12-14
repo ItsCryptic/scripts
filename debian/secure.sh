e32499d0d78362efac951aede93ffaf895ce7c13
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-12-13 19:56:42 -0500

    Remove shit about my school

--- debian/secure.sh
+++ debian/secure.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 echo "By default this script blocks ports 25, 587 and 465. If you need to allow these ports for e-mail or similar. Please run ufw allow 25,465,587."
-echo "You should also run sudo -s (gains access to root account) and apt update && apt upgrade -y peridodically to get the latest security updates, I'd run this everytime you come into shop, probably at the end of the week."
+echo "You should also run sudo -s (gains access to root account) and apt update && apt upgrade -y peridodically to get the latest security updates"
 echo "This will also install apache2. If apache is already installed by this script and you run this anyway you may get an `already installed` message. You can safely ignore that message."
 
 sudo -s
