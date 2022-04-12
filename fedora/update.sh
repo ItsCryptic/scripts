b28529d2b2619f19f38979db2cb91acf2114afc0
Kevin D <kevin.diangelis@gmail.com>
2022-04-12 08:58:51 -0400

    add update script for fedora

---
+++ fedora/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo dnf update -y
+notify-send -t 300 -i face-smile "Updates completed." "<b>Please reboot.</b>"
--- login/login.sh
+++ login/login.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 screen premid
 emacs --daemon
